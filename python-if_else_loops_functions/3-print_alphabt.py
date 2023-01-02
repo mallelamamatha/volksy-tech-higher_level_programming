1fb4cdedaf4d47073e196b4482d264c4e94cada1
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-01 23:49:21 -0800

    3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i == 101 or i == 113:
+for i in range(ord('a'), ord('z')+1):
+    if i ==ord('q') or i == ord('e'):
         continue
-    print(chr(i), end="" .format(chr))
+    print("{:c}".format(i), end="")
