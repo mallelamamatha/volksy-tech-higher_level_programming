8b11019e17d4e20fc31977c46f08b1578b3094ad
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-02 04:49:08 -0800

    12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,5 +5,7 @@ def fizzbuzz():
             print("Fizz", end=" ")
         elif i % 3 == 0:
             print("Fizz", end=" ")
+        elif i % 5  = 0:
+            print("Buzz", end=" ")
         else:
             print(i, end=" ")
