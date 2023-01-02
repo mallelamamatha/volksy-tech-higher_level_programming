8b1da37ba731f47251dae51c309b338a5e4bba74
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-01 23:18:32 -0800

    1

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+# YOUR CODE HERE
+last = number % 10
+if number < 0:
+    last = last - 10
+if last > 5:
+    print("last digit of",number,"is",last, "and is greater than 5")
+elif last == 0:
+    print("last digit of",number,"is",last,"and is zero")
+else:
+    print("last digit of",number,"is",last,"and is less than 6 and not 0")
