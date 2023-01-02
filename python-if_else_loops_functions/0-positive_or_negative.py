3bca36f2c69cab1b4d585a40667c9409d4059a67
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-01 22:03:33 -0800

    0

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,9 +4,9 @@ number = random.randint(-10, 10)
 # YOUR CODE HERE
 a=int(input("enter number:"))
 if a > 0:
-    print("positive")
+    print("number is positive")
 elif a == 0:
-    print("zero")
+    print("number is zero")
 else a < 0:
-    print("negative")
+    print("number is negative")
 
