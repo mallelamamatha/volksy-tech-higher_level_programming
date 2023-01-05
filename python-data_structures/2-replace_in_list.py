e8234b64eafad5ee50874e89aef061235f92f463
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-05 03:38:24 -0800

    2

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < my_list:
+        return original list
+    elif idx >= my_list:
+        return original list
+    return my_list[idx] = element
