b969d5b61f48419049955cf2794785091fa290a5
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-05 22:48:18 -0800

    9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list ==0 or my_list !=max(my_list):
+    if my_list == 0 or my_list != 0 max(my_list):
         return my_list
-    max(my_list)
+    max(my_list[my_list])
     return my_list
