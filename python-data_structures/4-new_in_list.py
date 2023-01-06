4ea2edca8c7219dc0ea3f3bac6b870034ce7db20
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-05 20:50:25 -0800

    4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     copy = my_list
-    if  idx < o:
+    if idx < 0:
         return my_list
     elif idx >= len(my_list):
         return my_list
