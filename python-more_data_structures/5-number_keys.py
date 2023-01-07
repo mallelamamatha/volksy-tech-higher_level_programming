7612c3498cd4cf5a4de6885cd7f162df3172107b
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-06 20:52:10 -0800

    5

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    # if c is not in c
+    lst = []
+    for x in my_string:
+        if x != 'c' and x != 'c':
+            lst.append(x)
+    return "".join(lst)
+
