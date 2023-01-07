8868bd00bddf20551d2561099b52f78c9d970fd2
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-06 20:59:56 -0800

    5

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    # if c is not in c
-    lst = []
-    for x in my_string:
-        if x != 'c' and x != 'c':
-            lst.append(x)
-    return "".join(lst)
-
+def number_keys(a_dictionary):
+    return(len(a_dictionary))
