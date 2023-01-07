183060ff8de4c43be6d0dbad5274a03577341bca
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-06 20:39:45 -0800

    3

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -2,9 +2,6 @@
 def common_elements(set_1, set_2):
     lst = []
     for i in set_1:
-        if i not in set_2:
-            lst.append(i)
-    for i in set_2:
-        if i not in set_1:
+        if i in set_2:
             lst.append(i)
     return lst
