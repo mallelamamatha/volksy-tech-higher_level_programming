06941f18ac49a2acaa833e185ab1fc72aef0496e
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-08 02:54:16 -0800

    6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
+def 
+print_sorted_dictionary(a_dictionary):
     j = 0
     for i in sorted(a_dictionary):
-        print("{}:{}".format(i, a_dictionary[i]))
+        print("{}:{}".format(i,
+    a_dictionary[i]))
