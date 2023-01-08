6ef20d410ba85e9116dce6b098f9740920c09f81
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-08 08:10:12 -0800

    12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    rom_num = {'I' : 1,
-               'V' : 5,
-               'X' : 10,
-               'L' : 50,
-               'C' : 100,
-               'D' : 500,
-               'M' : 1000
+    rom_num = {'I': 1,
+               'V': 5,
+               'X': 10,
+               'L': 50,
+               'C': 100,
+               'D': 500,
+               'M': 1000
                }
     if roman_string is None or type(roman_string) is not str:
         return 0
