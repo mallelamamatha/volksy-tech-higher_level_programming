8f585d6f697bc10d60af7e094d00fa159f97a541
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-09 04:55:26 -0800

    3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a/ b
+        result = a / b
         return result
     except:
         result = None
         return result
     finally:
-        print("Inside result:{}".format(result))
+        print("Inside result: {}".format(result))
