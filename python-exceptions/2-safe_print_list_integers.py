a05bb578348cdd05b46bf88f4c20896a42d871ad
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-09 04:39:21 -0800

    2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-                    e += 1
+            e += 1
         except (ValueError, TypeError):
             continue
         except (IndexError):
