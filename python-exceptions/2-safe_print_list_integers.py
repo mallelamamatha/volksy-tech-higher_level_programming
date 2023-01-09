4493087d615db61acca218c70642f502e8fc6624
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-09 04:16:32 -0800

    2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    y = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[y])
+                    y += 1
+        except (ValueError, IndexError):
+            continue
+        except (KeyError):
+            raise
+    print()
+    return y
