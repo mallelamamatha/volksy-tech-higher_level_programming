c40c2430816d2d8bf749973b63a014a673b4d76a
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-09 03:00:54 -0800

    0

---
+++ 0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    a = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[a]), end="")
+            a += 1
+        except:
+            pass
+        print('')
+        return a
