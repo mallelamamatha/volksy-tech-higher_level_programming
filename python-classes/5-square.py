233088e7ac0917a940a0a84ac24325c13a695074
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-11 05:06:55 -0800

    5

---
+++ python-classes/5-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class Square:
+    """Printing a square"""
+
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        """constructor"""
+
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        """area of the square"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """empty file"""
+        print(self)
