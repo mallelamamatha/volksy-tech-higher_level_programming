62be70ea5526f9bd090344f7783614bf0b4c5a8f
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-11 08:39:27 -0800

    5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -25,4 +25,6 @@ class Square:
         return (self.__size * self.__size)
 
     def my_print(self):
-        print(self)
+        print(self, size = 0)
+        s = Square(size, 0)
+        s.my_print()
