abe9c55ea08e4d2c7535385dcf6e50a928a97ee7
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-10 20:04:20 -0800

    3

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,14 +3,13 @@
 
 
 class Square:
-    """square size"""
-    def __init__(self, size=0):
-        """constuctor"""
+    """ Access and update private attribute"""
+    def size(self):
+        """constructor"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        slef.__size = size
 
-    def area(self):
-        """area of a square"""
-        return (self.__size * self.__size)
+
