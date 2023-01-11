f18d2a73d8ddbe958372dff0db36f40abb879c73
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-10 20:31:14 -0800

    4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -12,4 +12,10 @@ class Square:
             raise ValueError("size must be >= 0")
         slef.__size = size
 
+    def __init__(self, size=0):
+        """constructor"""
+        slef.__size = 0
 
+    def area(self):
+        """area of square"""
+        return (self.__size * self.__size
