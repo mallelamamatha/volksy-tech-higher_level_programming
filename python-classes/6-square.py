d4e2c19ec1a771feaacc3e47555ee1905bf9aedb
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-11 22:36:49 -0800

    6

---
+++ python-classes/6-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""string"""
+
+
+class Square:
+    """class square"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+      if type(value) != int:
+         raise TypeError("size must be an intege")
+      elif value < 0:
+         raise valueError("size must be >= 0")
+      self.__size = value
+
+     @position.setter
+     def position(self, value):
+       if type(value) != tuple or len(value) != 2:
+         raise TypeError("position must be a tuple of 2 positive integers")
+       if value[0] < 0 or value[1] < 0:
+          raise TypeError9"position must be a tuple of 2 positive integers")
+        self.__position = value
+
+     def area(self):
+       a = self.__size * self.__size
+       return a
+
+   def my_print(self):
+       if self.__size == 0:
+          print("")
+       else:
+           for line in range(0, self.__position[1]):
+               print()
+               for i in range(0, self.__size):
+                   for space in range(0, self.__position[0]):
+                       print(" ",end="")
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
