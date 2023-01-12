c211d36cb90ace17a5236eb4268e2301bbc7b7f0
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-11 20:52:00 -0800

    5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -29,4 +29,4 @@ class Square:
         for i in range(0, self.__size):
             for j in range(0, self.__size):
                 print('#', end="")
-            print()
+                print()
