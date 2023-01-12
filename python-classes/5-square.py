6878a608ff6f4c694170c5d78a124b59284be461
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-11 20:48:58 -0800

    5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -4,7 +4,6 @@
 
 class Square:
     """Printing a square"""
-
     def size(self):
         return self.__size
 
@@ -25,9 +24,9 @@ class Square:
         return (self.__size * self.__size)
 
     def my_print(self):
-        if self.__size == 0:
+      if self.__size == 0:
             print("")
         for i in range(0, self.__size):
             for j in range(0, self.__size):
                 print('#', end="")
-                print()
+            print()
