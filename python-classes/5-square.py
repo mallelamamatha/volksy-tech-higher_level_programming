a96eb262f5031ad8d84cd096ee5f130d5b15d6b4
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-11 20:56:39 -0800

    5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
-"""class"""
+"""creating class"""
 
 
 class Square:
-    """Printing a square"""
-    def size(self):
-        return self.__size
-
-    def size(self, value):
-        """constructor"""
-
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
+    """Square with private instance attribute and instantiation"""
     def __init__(self, size=0):
-        self.size = size
+        """Initialize data"""
+        self.__size = size
 
     def area(self):
-        """area of the square"""
-        return (self.__size * self.__size)
+        """Return square area"""
+        return (self.__size ** 2)
 
     def my_print(self):
-      if self.__size == 0:
-            print("")
-        for i in range(0, self.__size):
-            for j in range(0, self.__size):
-                print('#', end="")
+        """Print square with # to stdout"""
+        if self.__size <= 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end='')
                 print()
+
+    @property
+    def size(self):
+        """Retrieve size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Sets size and handles errors"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0"
