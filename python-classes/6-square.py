3754c5ba0ee57b3b5274d68d0c3cdcd78cb6e1a2
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-12 00:41:59 -0800

    hi

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """class square"""
-    def _init_(self, size=0, position=(0, 0)):
+    def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
 
