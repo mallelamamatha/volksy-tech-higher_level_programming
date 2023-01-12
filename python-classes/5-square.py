f73c6397f265252bfa3e443480b7de1fef3f6c06
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-11 21:24:46 -0800

    hi

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -33,4 +33,4 @@ class Square:
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError("size must be >= 0"
+            raise ValueError("size must be >= 0")
