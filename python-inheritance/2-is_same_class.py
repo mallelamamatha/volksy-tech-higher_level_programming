6d32aefdac7749f464c8ba5ffcac65e1fbeb7755
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-12 23:56:35 -0800

    2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""MyList
-"""
+# 2-is_same_class.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a class-checking function."""
 
 
-class MyList(list):
-    """Contains list
+def is_same_class(obj, a_class):
+    """Check if an object is exactly an instance of a given class.
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
     """
-
-    def print_sorted(self):
-        """Prints self in sorted format
-        """
-
-        print(sorted(self))
+    if type(obj) == a_class:
+        return True
+    return False
