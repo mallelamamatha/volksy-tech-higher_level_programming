25616af425c7294f5dcb437a7bc24e6538fa4e8f
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-18 04:01:45 -0800

    6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''str'''
+import json
+
+
+def load_from_json_file(filename):
+    '''file name'''
+    with open(filename, "w") as myfile:
+        return(myfile.write(json.dumps(filename))
