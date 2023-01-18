3220cf15fdbddc50b70c8a3a7e5c666912b3d133
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-18 02:25:02 -0800

    0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''str'''
+
+
+def read_file(filename=""):
+    '''file name'''
+    with open("filename="".txt",mode="w",encoding="utf8") as myfile:
+        print(myfile.read(), end="")
