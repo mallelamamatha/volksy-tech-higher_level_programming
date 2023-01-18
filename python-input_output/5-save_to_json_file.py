e655c051bd37ca06add6a403e97161a1a2c2b620
mallelamamatha <mamatham@brightcomgroup.com>
2023-01-18 03:46:40 -0800

    5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     '''file name'''
     with open(filename, mode="w") as myfile:
-    json.dumps(my_obj, myfile)
+        json.dumps(my_obj, myfile)
