e8efd234cff063ca3ebb9007ccae50f503bb139c
mallelamamatha <mamatham@brightcomgroup.com>
2023-02-01 01:25:06 -0800

    4

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- create the table id_not_null.
-CREATE TABLE IF NOT EXISTS 'id_not_null(id int DEFAULT 1,NAME VARCHAR(256));
+CREATE TABLE IF NOT EXISTS 'id_not_null(id INT DEFAULT 1, name VARCHAR(256));
