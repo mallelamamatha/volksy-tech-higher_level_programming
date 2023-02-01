38617b5a8813791a69d6131b01bcd362166d5d18
mallelamamatha <mamatham@brightcomgroup.com>
2023-02-01 01:33:26 -0800

    1

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,2 +1,6 @@
 -- table force_name.
-CREATE TABLE IF NOT EXISTS force_name(id int, name VARCHAR(256) NOT NULL);
+-- Creates the table force_name.
+CREATE TABLE IF NOT EXISTS `force_name` (
+    `id`   INT,
+    `name` VARCHAR(256) NOT NULL
+);
