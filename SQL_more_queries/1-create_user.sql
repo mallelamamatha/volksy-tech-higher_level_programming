4c7b563429c5b9346a547a667759c66e4a490d6a
mallelamamatha <mamatham@brightcomgroup.com>
2023-02-01 20:37:13 -0800

    1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- Creates the user user_0d_1 with all privilege.
+1-TASK: to create  new user and give all grants
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON . TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
