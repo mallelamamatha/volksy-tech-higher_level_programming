9cf6c2943c8155fa78e9d3baacdbf810b3b3a535
mallelamamatha <mamatham@brightcomgroup.com>
2023-02-01 20:42:20 -0800

    1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
-1-TASK: to create  new user and give all grants
+-- to create  new user and give all grants
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON . TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
