acec3532294060402f9a0c7a3058538d34e44581
mallelamamatha <mamatham@brightcomgroup.com>
2023-02-06 23:12:17 -0800

    0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
+
 import sys
 import MySQLdb
+
 if __name__ == "__main__";
     db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
