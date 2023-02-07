c9eead57707fc6142d864a4636747077b319fce0
mallelamamatha <mamatham@brightcomgroup.com>
2023-02-06 23:08:17 -0800

    0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+if __name__ == "__main__" ;
+db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
+c = db.cursor()
+c.execute('select * from states')
+[print(state) for state in c.fetchall()]
